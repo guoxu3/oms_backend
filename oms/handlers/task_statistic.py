b84605a0b3f185c5c61eff6629356c344ecc6728
GuoXu <guoxu3@qq.com>
2017-03-16 14:35:15 +0800

    修复小bug

--- oms/handlers/task_statistic.py
+++ oms/handlers/task_statistic.py
@@ -39,9 +39,11 @@ class TaskStatisticHandler(tornado.web.RequestHandler):
         begin_time = int(self.get_argument('begin_time', 1483200000))
         end_time = int(self.get_argument('end_time', 4102415999))
         is_all = self.get_argument('is_all', False)
+
         if is_all:
             task_statistic_info, user_list = db_utils.get_all_user_task_statistic_by_time(begin_time, end_time)
         else:
+            user_list = False
             task_statistic_info = db_utils.get_task_statistic_by_time(begin_time, end_time, username)
 
         if task_statistic_info != False:
